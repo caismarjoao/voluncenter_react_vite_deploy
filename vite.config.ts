3ffbfa8813ca9fcaef42eb0fd40adc18786d9864
João Cascais <jpmcascais@gmail.com>
2024-12-06 12:32:43 +0000

    configurations of deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()]
+  plugins: [tsconfigPaths(), react()],
+  base: "/voluncenter_react_vite_deploy",
 })
